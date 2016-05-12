052294b00adf45c1ed72bc9fae365101513c8e2f
Fred Thomsen <fredthomsen@fredthomsen.net>
2016-05-12 09:19:29 -0400

    Start laying out file structure and classes

---
+++ multicodec/__init__.py
@@ -0,0 +1,17 @@
+"""
+Python Multicodec Library
+~~~~~~~~~~~~~~~~~~~~~
+
+
+
+:copyright: (c) 2016 by Fred Thomsen.
+:license: Apache 2.0, see LICENSE for more details.
+
+"""
+
+__title__ = 'py-multicodec'
+__version__ = '0.0.1'
+__build__ = 0x000001
+__author__ = 'Fred Thomsen'
+__license__ = 'Apache 2.0'
+__copyright__ = 'Copyright 2016 Fred Thomsen'
---
+++ multicodec/multicodec.py
@@ -0,0 +1,17 @@
+"""
+multicodec
+~~~~~~~~~~~~~~~~~~~~~
+
+
+"""
+
+def encode(fmt, in_str):
+    pass
+
+def decode(fmt, in_str):
+    pass
+
+class multicodec(object):
+    def __init__(self):
+        self._header = None
+        self._codec = None
