85473c53f8609b1e5116b8be3f4ffd339e14bdec
Fred Thomsen <fredthomsen@fredthomsen.net>
2016-05-28 18:37:01 -0400

    Add json encoder and decoder

--- multicodec/json.py
+++ multicodec/json.py
@@ -1,26 +1,60 @@
 """
-json
+mutlicodec.json
 ~~~~~~~~~~~~~~~~~~~~~
 
-
+JSON Multicodec encoder and decoder.
 """
+
 import json
-import mutlicodec.coder
-import multicodec.header
+import copy
+
+from mutlicodec import coder
+from multicodec import header
+from multicodec import exceptions
 
 FORMAT = '/json/'
+DECODED_DICT_FMT = {"codec": "json", "data": ""}
+
 
 class Encoder(coder.Encoder):
+    """
+    JSON Encoder.
+    """
+
     def __call__(self, buf):
+        """
+        JSON Multicodec encoder functionality.
+
+        :params buf: JSON string.
+
+        :return: JSON Multicodec buffer.
+        """
+
         try:
             json.loads(buf)
         except ValueError:
-            raise MalformedBufferError
+            raise exceptions.MalformedBufferError
 
-        header.write_header(self._writer, FORMAT)
+        encoded = header.add_header(buf, FORMAT)
+        return encoded
 
-        return mc_buffer
 
 class Decoder(coder.Decoder):
+    """
+    JSON Decoder.
+    """
+
     def __call__(self, buf):
-        header.
+        """
+        JSON Multicodec decoder functionality.
+
+        :params buf: JSON multicodec buffer.
+
+        :return: Dictionary containing JSON string.
+        """
+
+        buf = header.rm_header(buf)
+        decoded = copy.deepdcopy(DECODED_DICT_FMT)
+        decoded['data'] = buf
+
+        return decoded
