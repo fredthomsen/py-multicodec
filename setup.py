9644229ff502a3bcfe94b1c05018d5e716b79621
Fred Thomsen <fredthomsen@fredthomsen.net>
2016-06-26 15:39:57 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,3 +4,11 @@ except ImportError:
     from distutils.core import setup
 
 version = '0.0.1'
+
+setup(
+    name='py-multicodec',
+    version=version,
+    description='Self-describing serialization. This is a python implementation of https://github.com/jbenet/multicodec',
+    author='Fred Thomsen',
+    license='Apache License'
+)
