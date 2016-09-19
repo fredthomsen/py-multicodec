8c7ec80867eb622aaff663668783a3b956307246
Fred Thomsen <fredthomsen@fredthomsen.net>
2016-09-19 10:26:47 -0400

    Add nested test... makes it clear that multicodec decode needs to be able to decode more than once

--- test/test_multicodec.py
+++ test/test_multicodec.py
@@ -6,11 +6,12 @@ Unit tests for multicodec.
 """
 
 import json
+import cbor
 
 from multicodec.multicodec import MultiCodec
 from multicodec.multicodec import encode
 from multicodec.multicodec import decode
-from multicodec.multicodec import JSON_FORMAT
+from multicodec.multicodec import JSON_FORMAT, CBOR_FORMAT
 
 from multicodec import coder_json
 
@@ -30,3 +31,11 @@ def test_encode():
 def test_decode():
     json_mc_buf = b'\x07\x2f\x6a\x73\x6f\x6e\x2f\x0a\x7b\x22\x68\x65\x6c\x6c\x6f\x22\x3a\x20\x22\x77\x6f\x72\x6c\x64\x22\x7d'
     assert decode(json_mc_buf) == {"codec": "json", "data": '{"hello": "world"}'}
+
+
+def test_nested():
+    json_str = json.dumps({"hello": "world"})
+    json_mc_buf = encode(JSON_FORMAT, json_str)
+    cbor_mc_buf = encode(CBOR_FORMAT, json_mc_buf)
+
+    assert json_str == decode(cbor_mc_buf)
