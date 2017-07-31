d44cfa173cf586ce663e1a2be918cef292658ce7
Fred Thomsen <me@fredthomsen.net>
2017-07-31 09:26:19 -0400

    Move multicodec tests to use hypothesis

--- test/test_multicodec.py
+++ test/test_multicodec.py
@@ -5,14 +5,14 @@ test.test_multicodec
 Unit tests for multicodec.
 """
 
-import json
 import cbor
+import json
+from hypothesis import strategies, given
 
 from multicodec.multicodec import MultiCodec
 from multicodec.multicodec import encode
 from multicodec.multicodec import decode
 from multicodec.multicodec import JSON_FORMAT, CBOR_FORMAT
-
 from multicodec import coder_json
 
 
@@ -22,20 +22,25 @@ def test_build():
             isinstance(json_mcoder._encoder, coder_json.Encoder) and
             isinstance(json_mcoder._decoder, coder_json.Decoder))
 
+mc_strategy = strategies.dictionaries(
+    keys=strategies.text(
+        alphabet='abcdefghijklmnopqrstuvwxyz1234567890',
+        min_size=10),
+    values=strategies.text(min_size=10),
+    min_size=10, max_size=30)
+@given(mc_dict=mc_strategy)
+def test_rtt(mc_dict):
+    assert decode(encode(JSON_FORMAT, json.dumps(mc_dict))) == {
+            "codec": "json", "data": json.dumps(mc_dict)
+    }
 
-def test_encode():
-    json_str = json.dumps({"hello": "world"})
-    assert encode(JSON_FORMAT, json_str) == b'\x07\x2f\x6a\x73\x6f\x6e\x2f\x0a\x7b\x22\x68\x65\x6c\x6c\x6f\x22\x3a\x20\x22\x77\x6f\x72\x6c\x64\x22\x7d'
 
-
-def test_decode():
-    json_mc_buf = b'\x07\x2f\x6a\x73\x6f\x6e\x2f\x0a\x7b\x22\x68\x65\x6c\x6c\x6f\x22\x3a\x20\x22\x77\x6f\x72\x6c\x64\x22\x7d'
-    assert decode(json_mc_buf) == {"codec": "json", "data": '{"hello": "world"}'}
-
-
-def test_nested():
-    json_str = json.dumps({"hello": "world"})
+@given(mc_dict=mc_strategy)
+def test_nested(mc_dict):
+    json_str = json.dumps(mc_dict)
     json_mc_buf = encode(JSON_FORMAT, json_str)
     cbor_mc_buf = encode(CBOR_FORMAT, json_mc_buf)
 
-    assert {'codec': 'cbor', 'data': {'codec': 'json', 'data': '{"hello": "world"}'}} == decode(cbor_mc_buf)
+    assert {'codec': 'cbor', 'data': {
+        'codec': 'json', 'data': '{0}'.format(json_str)
+    }} == decode(cbor_mc_buf)
